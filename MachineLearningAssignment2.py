08431bf5f338e01edb6966f543dcb5e27e26feeb
Cian O'Sullivan <cjp1805@outlook.com>
2020-11-27 22:09:46 +0000

    Removing unesscary old comments

--- MachineLearningAssignment2.py
+++ MachineLearningAssignment2.py
@@ -40,26 +40,6 @@ def preprocess():
     
     # print(product_df.columns)    
     
-    # Did a quick check for empty values and found none    
-    # for col in product_df.columns:
-    #     print(col, ": " , product_df[col].isnull().sum())
-    
-    # Split the dataset into labels and features
-    # This seems to create a new dataframe of each column except label
-    # feature_vectors = product_df.loc[:, product_df.columns != 'label']
-    # labels = product_df['label']
-    
-    # Need features and target to be in this format of numpy array
-    # Digit target:  [0 1 2 ... 8 9 8]
-    # Digit data:  
-    # [[ 0.  0.  5. ...  0.  0.  0.]
-    #  [ 0.  0.  0. ... 10.  0.  0.]
-    #  [ 0.  0.  0. ... 16.  9.  0.]
-    #  ...
-    #  [ 0.  0.  1. ...  6.  0.  0.]
-    #  [ 0.  0.  2. ... 12.  0.  0.]
-    #  [ 0.  0. 10. ... 12.  1.  0.]]
-    
     # Adding .values at the end as it then returns a numpy array
     feature_vectors = product_df.loc[:, product_df.columns != 'label'].values
     labels = product_df['label'].values
@@ -71,41 +51,18 @@ def preprocess():
     
 
     # Example Sneaker
-    # plt.figure(1, figsize=(3, 3))
-    # plt.imshow(np.reshape(feature_vectors.values[3], (28, 28)), cmap='gray', interpolation='nearest')
-    # plt.show()
     plt.figure(1, figsize=(3, 3))
     plt.imshow(np.reshape(feature_vectors[3], (28, 28)), cmap='gray', interpolation='nearest')
     plt.show()
     
     # Example Ankle Boot
-    # plt.figure(1, figsize=(3, 3))
-    # plt.imshow(np.reshape(feature_vectors.values[0], (28, 28)), cmap='gray', interpolation='nearest')
-    # plt.show()
     plt.figure(1, figsize=(3, 3))
     plt.imshow(np.reshape(feature_vectors[0], (28, 28)), cmap='gray', interpolation='nearest')
     plt.show()
      
-    # The number of rows taht will be used as samples
-    # Initially starting with 200 of each
-    # no_samples = 200
     
-    # # Samples of feature vector and labels
-    # fv_samples = feature_vectors[:no_samples]
-    # labels_samples = labels[no_samples]
-    
-    # Split the data into train and test so its parametised
-    # train_data = feature_vectors[0:int(0.8*len(feature_vectors))]
-    # train_target = labels[0:int(0.8*len(labels))]
-    # test_data = feature_vectors[int(0.8*len(feature_vectors)):len(feature_vectors)]
-    # test_target = labels[int(0.8*len(labels)):len(labels)]
-    
-    # Sample variable for how much to take
-    # samples tested: 0.08, 0.8
-    # no_samples = 0.08
+    # Sample variable for the amount of samples form the feature vector to use
     no_samples = 0.8
-    # no_samples = 0.04
-    # no_samples = 0.4
     
     
     # Starting off with reducted amount
@@ -383,7 +340,7 @@ def svm_func(train_data, train_target, test_data, test_target, no_samples, no_sp
     print("Prediciton accuracy score for Linear:", metrics.accuracy_score(test_target, best_linear.predict(test_data)))
     
     
-        # Calculate the minimum, the maximum, and the average of the training time per training sample [1 point], 
+    # Calculate the minimum, the maximum, and the average of the training time per training sample [1 point], 
     # the prediction time per evaluation sample [1 point]     
     print("Minimum Linear training time per training sample:", min(linear_training_times)/no_samples, "secs")
     print("Maximum Linear training time per training sample:", max(linear_training_times)/no_samples, "secs")
@@ -398,12 +355,12 @@ def svm_func(train_data, train_target, test_data, test_target, no_samples, no_sp
     
     # Calculate the minimum, the maximum, and the average of the training time per training sample [1 point], 
     # the prediction time per evaluation sample [1 point]     
-    # print("Minimum rbf training time per training sample:", min(rbf_training_times)/no_samples, "secs")
-    # print("Maximum rbf training time per training sample:", max(rbf_training_times)/no_samples, "secs")
-    # print("Average rbf training time per training sample:", (sum(rbf_training_times)/len(rbf_training_times))/no_samples, "secs")
-    # print("Minimum rbf predicition time per evaulation sample:", min(rbf_prediction_times)/no_samples, "secs")
-    # print("Maximum rbf predicition time per evaulation sample:", max(rbf_prediction_times)/no_samples, "secs")
-    # print("Average rbf predicition time per evaulation sample:", (sum(rbf_prediction_times)/len(rbf_prediction_times))/no_samples, "secs")
+    print("Minimum rbf training time per training sample:", min(rbf_training_times)/no_samples, "secs")
+    print("Maximum rbf training time per training sample:", max(rbf_training_times)/no_samples, "secs")
+    print("Average rbf training time per training sample:", (sum(rbf_training_times)/len(rbf_training_times))/no_samples, "secs")
+    print("Minimum rbf predicition time per evaulation sample:", min(rbf_prediction_times)/no_samples, "secs")
+    print("Maximum rbf predicition time per evaulation sample:", max(rbf_prediction_times)/no_samples, "secs")
+    print("Average rbf predicition time per evaulation sample:", (sum(rbf_prediction_times)/len(rbf_prediction_times))/no_samples, "secs")
     
     # # Calculate the minimum, the maximum, and the average of the training time per training sample [1 point], 
     # # the prediction time per evaluation sample [1 point]     
@@ -420,19 +377,19 @@ def svm_func(train_data, train_target, test_data, test_target, no_samples, no_sp
     
     # # Calculate the minimum, the maximum, and the average of the training time per training sample [1 point], 
     # # the prediction time per evaluation sample [1 point]     
-    print("Minimum rbf training time per training sample:", min(rbf_training_times), "secs")
-    print("Maximum rbf training time per training sample:", max(rbf_training_times), "secs")
-    print("Average rbf training time per training sample:", (sum(rbf_training_times)/len(rbf_training_times)), "secs")
-    print("Minimum rbf predicition time per evaulation sample:", min(rbf_prediction_times), "secs")
-    print("Maximum rbf predicition time per evaulation sample:", max(rbf_prediction_times), "secs")
-    print("Average rbf predicition time per evaulation sample:", sum(rbf_prediction_times)/len(rbf_prediction_times), "secs")
+    # print("Minimum rbf training time per training sample:", min(rbf_training_times), "secs")
+    # print("Maximum rbf training time per training sample:", max(rbf_training_times), "secs")
+    # print("Average rbf training time per training sample:", (sum(rbf_training_times)/len(rbf_training_times)), "secs")
+    # print("Minimum rbf predicition time per evaulation sample:", min(rbf_prediction_times), "secs")
+    # print("Maximum rbf predicition time per evaulation sample:", max(rbf_prediction_times), "secs")
+    # print("Average rbf predicition time per evaulation sample:", sum(rbf_prediction_times)/len(rbf_prediction_times), "secs")
     
     
 def main():
     train_data, train_target, test_data, test_target, no_samples = preprocess()    
     
-    # perceptron(train_data, train_target, test_data, test_target, no_samples, 32)
+    perceptron(train_data, train_target, test_data, test_target, no_samples, 2)
     
-    svm_func(train_data, train_target, test_data, test_target, no_samples, 4)
+    svm_func(train_data, train_target, test_data, test_target, no_samples, 2)
     
 main()
